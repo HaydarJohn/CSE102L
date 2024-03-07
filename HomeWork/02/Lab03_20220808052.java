5ba2ef2021580067b94bc7f46f5a057124e1e878
haydarjohn <haydarcan_2003@hotmail.com>
2024-03-07 20:39:03 +0300

    Changing comp

---
+++ HomeWork/02/Lab03_20220808052.java
@@ -0,0 +1,14 @@
+public class Lab_20220808052
+{
+    public static void main(String[] args)
+    {
+        if(args[0].equals("Merhaba"))
+        {
+            System.out.println("Merhaba")
+        }
+        else
+        {
+            System.out.println("Anlamadim. Tekrar de");
+        }
+    }
+}
